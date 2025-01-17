eaaac044990a28e7a28ede22dc90c7b4e9bba5c3
xuanphong230104 <nguyenxuanphong230104@gmail.com>
2025-01-17 08:58:38 +0700

    test

--- src/components/ActivateTag/index.jsx
+++ src/components/ActivateTag/index.jsx
@@ -2,7 +2,7 @@ import { Tag } from "antd";
 import PropTypes from "prop-types";
 
 const ActiveTag = ({ active }) => (
-  <Tag color={active ? "success" : "error"}>{active ? "True" : "False"}</Tag>
+  <Tag color={active ? "success" : "error"}>{active ? "Trueeeeee" : "False"}</Tag>
 );
 
 ActiveTag.propTypes = {
